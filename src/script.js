6dcc72575032f1faf1f4e8bee82c88444e897944
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2025-04-29 12:51:32 -0300

    the link of the GitHub repository was added

---
+++ src/script.js
@@ -0,0 +1,12 @@
+/*
+function showAlert(event) {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY hh:mm A");
+    alert(`It is ${currentTime} in ${event.target.value}`);
+  }
+}
+let selectElement = document.querySelector("#countrys");
+selectElement.addEventListener("change", showAlert);
+*/
